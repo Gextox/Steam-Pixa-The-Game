879bb66ae7d24982596e29e987c5fd7390412c78
Nicolas Sauco <59623002+Deven2@users.noreply.github.com>
2020-02-07 10:48:42 -0300

    Add files via upload

--- Steam-Pixa-The-Game-master/stats.js
+++ Steam-Pixa-The-Game-master/stats.js
@@ -2,8 +2,8 @@ let screen = document.querySelector("#gameArea")
 let mainBar = document.querySelector("#main_bar");
 let canvas = document.querySelector("#myCanvas");
 
-screen.style.height = window.innerHeight;
-screen.style.width = wondow.innerWidth;
+screen.style.height = window.innerHeight + "px";
+screen.style.width = window.innerWidth + "px";
 
 mainBar.style.width = canvas.width + "px";
 mainBar.style.height = (canvas.height / 3) + "px";
